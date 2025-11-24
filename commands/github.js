61e4d306c76601cc8d5d8ef428c7d344a5010d5d
chrisgaaju-coder <chrisgaajukpeme2027@gmail.com>
2025-11-24 14:03:13 +0100

    Update github.js

--- commands/github.js
+++ commands/github.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 async function githubCommand(sock, chatId, message) {
   try {
-    const res = await fetch('https://api.github.com/repos/chrisgaaju-coder/Mysterybot-md');
+    const res = await fetch('https://api.github.com/repos/chrisgaaju-coder/Mystery-MD');
     if (!res.ok) throw new Error('Error fetching repository data');
     const json = await res.json();
 
